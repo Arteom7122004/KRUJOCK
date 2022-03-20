60285032adbc113f7b004f72bd77223c797b50a3
Arteom7122004 <glushco7122004@gmail.com>
2022-03-21 00:06:38 +0300

    Function

--- src/Задание.kt
+++ src/Задание.kt
@@ -1,16 +1,24 @@
 fun main(arch: Array<String>) {
-    var names: Array<String> = arrayOf(" ", " ", " "," " ," " )
+    Names()
+    Print(Array<String>)
+}
+fun Names() {
+    var names: Array<String> = arrayOf(" ", " ", " ", " ", " ")
     var i: Int = 0
     while (i <= 4) {
-    print("Введите имя и фамилию: ")
-    var name = readLine().toString()
-    print("Введите возраст: ")
-    val age = readLine()!!.toInt()
-    if (age >= 18) {
-        names[i] = name
-        i = i + 1
+        print("Введите имя и фамилию: ")
+        var name = readLine().toString()
+        print("Введите возраст: ")
+        val age = readLine()!!.toInt()
+        if (age >= 18) {
+            names[i] = name
+            i = i + 1
 
+
+        }
     }
-    }
-    println(names.contentToString())
+}
+fun Print(Names: Array<String>) {
+    println(Names())
+
 }
